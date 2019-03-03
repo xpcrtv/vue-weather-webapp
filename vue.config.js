cc8e8e344a3b01226b6b93874b5ca7ba07ff6731
xpcrtv <20889274+xpcrtv@users.noreply.github.com>
2019-03-03 12:59:52 +0500

    Add color settings for SW generation

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   pwa: {
     name: 'Weather Vue App',
-    appleMobileWebAppStatusBarStyle: 'black'
-  }
-}
+    appleMobileWebAppStatusBarStyle: 'black',
+    themeColor: '#b7dcd5',
+    msTileColor: '#b7dcd5',
+  },
+};
