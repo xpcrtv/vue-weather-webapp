481f61626b2d962ac63a035ffbca07f9f170c261
xpcrtv <20889274+xpcrtv@users.noreply.github.com>
2019-03-08 13:13:54 +0500

    Import vuetrend to project

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue';
+import Trend from 'vuetrend';
 import App from './App.vue';
 import './registerServiceWorker';
 
 Vue.config.productionTip = false;
+Vue.use(Trend);
 
 new Vue({
   render: h => h(App),
