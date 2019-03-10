2d648fe187f40bf2ff413e0db460db32b9160082
xpcrtv <20889274+xpcrtv@users.noreply.github.com>
2019-03-10 18:46:03 +0500

    Add axios and vuex

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue';
 import Trend from 'vuetrend';
 import App from './App.vue';
+import store from './store'
 import './registerServiceWorker';
 
 Vue.config.productionTip = false;
 Vue.use(Trend);
 
 new Vue({
-  render: h => h(App),
+  store,
+  render: h => h(App)
 }).$mount('#app');
