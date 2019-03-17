e5e55bb890cc051dd32218bbaf9b01a8d81b4b11
xpcrtv <20889274+xpcrtv@users.noreply.github.com>
2019-03-17 14:12:10 +0500

    Add app header component with basic logic

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="app-header">
+    <button role="button" class="btn" @click="g">
+      <span class="btn__icon">
+        <img src="/img/icons/geolocation.svg">
+      </span>
+    </button>
+    <button role="button" class="btn"  @click="r">
+      <span class="btn__icon">
+        <img src="/img/icons/refresh.svg" alt>
+      </span>
+      <slot></slot>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    r() {
+      this.$emit('refresh');
+    },
+    g() {
+      this.$emit('geo');
+    },
+  },
+};
+</script>
+
+<style>
+.app-header {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  width: 100%;
+}
+</style>
