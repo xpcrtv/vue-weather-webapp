4ff8b7152574d69e24ecd0c1b488d64b8c2bd445
xpcrtv <20889274+xpcrtv@users.noreply.github.com>
2019-03-17 19:43:23 +0500

    Add styles and logic to Weekly forecast component

--- src/components/AppWeekForecast.vue
+++ src/components/AppWeekForecast.vue
@@ -1,35 +1,44 @@
 <template>
   <div class="weekly-forecast">
+    <h2 class="app-title">Weekly forecast</h2>
     <ul class="weekly-list">
-      <li class="weekly-item" v-for="(day, index) in week" :key="index">
+      <li class="weekly-item" v-for="(day, index) in weather" :key="index">
         <div class="weekly-item__date">
           <div class="weekly-item__day-name">{{ formatToDayOfWeek(day.ts) }}</div>
           <div class="weekly-item__day">{{ formatToDayAndMonth(day.ts) }}</div>
         </div>
-        <div class="weekly-item__icon">{{ day.weather.icon }}</div>
+        <div class="weekly-item__icon">
+          <img :src="getWeatherIcon(day.weather.icon)" width="50" alt>
+        </div>
         <div class="weekly-info">
           <div class="weekly-info__item">
-            <div class="weekly-info__icon">min</div>
-            <div class="weekly-info__value">{{day.min_temp}}</div>
+            <div class="weekly-info__value">{{Math.round(day.min_temp)}} &deg;</div>
           </div>
           <div class="weekly-info__item">
-            <div class="weekly-info__icon">pop</div>
-            <div class="weekly-info__value">{{day.pop}}</div>
+            <div class="weekly-info__icon">
+              <img src="/img/weather-icons/drops.svg" alt>
+            </div>
+            <div class="weekly-info__value">{{day.pop}} %</div>
           </div>
           <div class="weekly-info__item">
-            <div class="weekly-info__icon">wind</div>
-            <div class="weekly-info__value">{{day.wind_spd}}</div>
+            <div class="weekly-info__icon">
+              <img src="/img/weather-icons/wind.svg" alt>
+            </div>
+            <div class="weekly-info__value">{{ Math.round(day.wind_spd) }} m/h</div>
           </div>
           <div class="weekly-info__item">
-            <div class="weekly-info__icon">max</div>
-            <div class="weekly-info__value">{{day.max_temp}}</div>
+            <div class="weekly-info__value">{{Math.round(day.max_temp)}} &deg;</div>
           </div>
           <div class="weekly-info__item">
-            <div class="weekly-info__icon">sunrise</div>
+            <div class="weekly-info__icon">
+              <img src="/img/weather-icons/sunrise.svg" alt>
+            </div>
             <div class="weekly-info__value">{{ formatToLocalTime(day.sunrise_ts)}}</div>
           </div>
           <div class="weekly-info__item">
-            <div class="weekly-info__icon">sunset</div>
+            <div class="weekly-info__icon">
+              <img src="/img/weather-icons/sunset.svg" alt>
+            </div>
             <div class="weekly-info__value">{{ formatToLocalTime(day.sunset_ts)}}</div>
           </div>
         </div>
@@ -39,34 +48,20 @@
 </template>
 
 <script>
+import getWeatherIcon from '../utils/weathericons.js';
 import dayjs from 'dayjs';
 
 export default {
-  data: () => ({
-    week: [
-      {
-        ts: 0,
-        sunrise_ts: 0,
-        sunset_ts: 0,
-        wind_spd: 0,
-        pop: 0,
-        max_temp: 0,
-        min_temp: 0,
-        weather: {
-          icon: '',
-        },
-      },
-    ],
-  }),
   props: {
-    weather: Object,
+    weather: Array,
   },
   watch: {
     weather(value) {
-      this.week = value.data;
+      this.week = value;
     },
   },
   methods: {
+    getWeatherIcon,
     formatToDayOfWeek(value) {
       return dayjs(value * 1000).format('dddd');
     },
@@ -74,11 +69,56 @@ export default {
       return dayjs(value * 1000).format('DD/MM');
     },
     formatToLocalTime(value) {
-      return dayjs(value * 1000).format('HH:mm');
+      return dayjs(value * 1000).format('LT');
     },
   },
 };
 </script>
 
 <style>
+.weekly-list {
+  padding: 0;
+  margin: 0;
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+}
+.weekly-item {
+  display: flex;
+  flex-wrap: nowrap;
+  margin: 5px 0;
+  padding: 10px 0;
+  border-bottom: 1px solid rgba(255, 255, 255, 0.35);
+}
+.weekly-item__date {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 80px;
+  font-size: 18px;
+  font-weight: bold;
+}
+.weekly-item__day-name {
+  width: 100%;
+  text-align: center;
+}
+.weekly-item__icon {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.weekly-info {
+  display: flex;
+  flex-wrap: wrap;
+  flex-grow: 1;
+}
+.weekly-info__item {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 33%;
+}
 </style>
