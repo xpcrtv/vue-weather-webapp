afd9e9817d417a686c607dc810149517b99f2ff8
xpcrtv <20889274+xpcrtv@users.noreply.github.com>
2019-03-17 19:46:31 +0500

    Add styles. Some changes in logic. Add icons to Day weather component

--- src/components/AppDayWeather.vue
+++ src/components/AppDayWeather.vue
@@ -4,36 +4,37 @@
     <div class="day-weather__update-time">last update: {{ formatedUpdateTime }}</div>
     <div class="day-weather__indicators">
       <div class="day-weather__item">
-        <span class="day-weather__icon"> => </span>
+        <span class="day-weather__icon">
+          <img src="/img/weather-icons/wind_spd.svg" width="30" alt>
+        </span>
         <span class="day-weather__value">{{ weather.wind_spd }} m/s</span>
       </div>
       <div class="day-weather__item">
         <span class="day-weather__icon">
-          <img :src='require(`@/assets/clouds.svg`)' width="30" alt="">
+          <img src="/img/weather-icons/clouds.svg" width="30" alt>
         </span>
         <span class="day-weather__value">{{ weather.clouds }}%</span>
       </div>
       <div class="day-weather__item">
-        <span class="day-weather__icon"> -> </span>
+        <span class="day-weather__icon">
+          <img src="/img/weather-icons/wind_dir.svg" width="30" alt>
+        </span>
         <span class="day-weather__value">{{ weather.wind_cdir }}</span>
       </div>
     </div>
-    <div class="day-weather__img"></div>
-    <div class="day-weather__temperature">{{ weather.temp }}</div>
+    <span class="day-weather__temperature">{{ Math.round(weather.temp) }}</span>
+    <span class="day-weather__description">{{ weather.weather.description }}</span>
     <div class="day-weather__indicators">
       <div class="day-weather__item">
         <span class="day-weather__icon">
-          <img :src='require(`@/assets/sunrise.svg`)' width="30" alt="">
+          <img src="/img/weather-icons/sunrise.svg" width="30" alt>
         </span>
         <span class="day-weather__value">{{ formatedSunriseTime }}</span>
       </div>
-      <div class="day-weather__item">
-        <span class="day-weather__icon"> )**) </span>
-        <span class="day-weather__value">{{ weather.pop }}</span>
-      </div>
+
       <div class="day-weather__item">
         <span class="day-weather__icon">
-          <img :src='require(`@/assets/sunset.svg`)' width="30" alt="">
+          <img src="/img/weather-icons/sunset.svg" width="30" alt>
         </span>
         <span class="day-weather__value">{{ formatedSunsetTime }}</span>
       </div>
@@ -42,10 +43,9 @@
 </template>
 
 <script>
-import dayjs from 'dayjs';
-import customParseFormat from 'dayjs/plugin/customParseFormat';
+import getWeatherIcon from "../utils/weathericons.js";
 
-dayjs.extend(customParseFormat);
+import dayjs from 'dayjs';
 
 const timeOffset = dayjs().utcOffset();
 
@@ -63,20 +63,78 @@ export default {
     formatedSunriseTime() {
       return dayjs(this.weather.sunrise, 'HH:mm')
         .add(timeOffset, 'minutes')
-        .format('HH:mm');
+        .format('LT');
     },
     formatedSunsetTime() {
       return dayjs(this.weather.sunset, 'HH:mm')
         .add(timeOffset, 'minutes')
-        .format('HH:mm');
+        .format('LT');
     },
     formatedUpdateTime() {
-      return dayjs(this.updateTime).format('DD.MM HH:mm');
+      return dayjs(this.updateTime).fromNow();
     },
   },
+  methods: {
+    getWeatherIcon
+  }
 };
 </script>
 
 <style>
-
+.day-weather {
+  flex-direction: column;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-top: 10px;
+  padding-bottom: 10px;
+}
+.day-weather__item {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+}
+.day-weather__icon {
+  margin: 0 10px;
+  width: 30px;
+}
+.day-weather__icon img {
+  width: 100%;
+}
+.day-weather__indicators {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  width: 100%;
+  margin: 10px 0;
+}
+.day-weather__city-name {
+  text-align: center;
+  font-size: 3em;
+}
+.day-weather__update-time {
+  font-size: 12px;
+  text-align: center;
+  margin-bottom: 10px;
+}
+.day-weather__temperature {
+  position: relative;
+  text-align: center;
+  font-size: 7em;
+  padding-right: 20px;
+  line-height: 1;
+}
+.day-weather__temperature::after {
+  content: '\00B0';
+  position: absolute;
+  top: 0;
+  right: 0;
+  font-size: 0.4em;
+}
+.day-weather__description {
+  font-size: 1.5em;
+  margin-bottom: 0.5em;
+}
 </style>
