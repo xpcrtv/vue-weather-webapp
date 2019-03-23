d97699ae33d7cc8793b504d56e04dc43c0e43ac8
xpcrtv <20889274+xpcrtv@users.noreply.github.com>
2019-03-23 22:06:26 +0500

    Add icons list to manifest. Add config for pwa plugin

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,18 @@
 module.exports = {
   pwa: {
     name: 'Weather Vue App',
+    appleMobileWebAppCapable: 'yes',
     appleMobileWebAppStatusBarStyle: 'black',
-    themeColor: '#b7dcd5',
+    themeColor: '#e0a7a1',
     msTileColor: '#b7dcd5',
+    workboxPluginMode: 'GenerateSW',
+    iconPaths: {
+      favicon32: 'img/icons/favicon-32x32.png',
+      favicon16: 'img/icons/favicon-16x16.png',
+      msTileImage: 'img/icons/mstile-150x150.png',
+      maskIcon: 'img/icons/safari-pinned-tab.svg',
+      appleTouchIcon: 'img/icons/apple-touch-icon.png',
+    },
   },
-
   lintOnSave: false,
 };
