d8e7db70b002e9a76939676196c774166c75ce74
xpcrtv <20889274+xpcrtv@users.noreply.github.com>
2019-03-24 22:25:29 +0500

    Change rules for PWA options

--- vue.config.js
+++ vue.config.js
@@ -5,13 +5,17 @@ module.exports = {
     appleMobileWebAppStatusBarStyle: 'black',
     themeColor: '#fafafa',
     msTileColor: '#b7dcd5',
-    workboxPluginMode: 'GenerateSW',
     iconPaths: {
       favicon32: 'img/icons/favicon-32x32.png',
       favicon16: 'img/icons/favicon-16x16.png',
       msTileImage: 'img/icons/mstile-150x150.png',
       maskIcon: 'img/icons/safari-pinned-tab.svg',
       appleTouchIcon: 'img/icons/apple-touch-icon.png',
+    },
+    workboxOptions: {
+      exclude: [
+        /\.map$/,
+      ],
     },
   },
   lintOnSave: false,
