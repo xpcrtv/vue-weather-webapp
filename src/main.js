f0c7f1bb9a41339db82a79895cf3db8248e0402e
Ruslan <g.experiens@gmail.com>
2019-09-27 20:51:55 +0500

    Move vuetrand declaration to component

--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
 import Vue from 'vue';
-import Trend from 'vuetrend';
 import App from './App.vue';
 import store from './store';
 import './registerServiceWorker';
 
 Vue.config.productionTip = false;
-Vue.use(Trend);
 
 new Vue({
   store,
